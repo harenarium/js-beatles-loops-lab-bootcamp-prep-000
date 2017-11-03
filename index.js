4c319043181ccf1945d447fa78e01e8498be5452
harenarium <carolinel2610@gmail.com>
2017-11-03 02:31:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(arrayMusicians, arrayInstruments){
   var array = []
 /*  for(n=0, n<arrayMusicians.length, n++){
   //  array.push(`arrayMusicians(n) plays arrayInstruments(n)`)
- } 
+ }
  */
   return array
 }
