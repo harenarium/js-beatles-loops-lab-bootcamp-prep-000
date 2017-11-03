982b55ccc99d85fa0a14bc2cccfb2bf03d9410a6
harenarium <carolinel2610@gmail.com>
2017-11-03 02:49:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,11 @@ function johnLennonFacts(array){
   }
   return array
 }
+
+function iLoveTheBeatles(number){
+  var array = []
+  do{
+    array.push("I love the Beatles!")
+    number++
+  }while(number<15)
+}
