fd5689edbc96932b81a2eba933b1603f064364cd
harenarium <carolinel2610@gmail.com>
2017-11-03 02:27:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(arrayMusicians, arrayInstruments){
   var array = []
-  for(n=0, n<arrayMusicians.length, n++){
-    array.push(`arrayMusicians[n] plays arrayInstruments[n]`)
+  for(n=0, n<arrayMusicians.length, ++n){
+    array.push(`arrayMusicians(n) plays arrayInstruments(n)`)
   }
   return array
 }
